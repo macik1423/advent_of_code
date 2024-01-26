3a39ab6657b25c455cffbebcab7623bb7fca1713
Kowalik <Maciej.Kowalik@durr.com>
2024-01-26 15:11:00 +0100

    add: 2017 day1 step2

---
+++ _advent_of_code/src/main/kotlin/_2017/day1/Step2.kt
@@ -0,0 +1,9 @@
+package _2017.day1
+
+fun main(args: Array<String>) {
+    val file = readFileAsLinesUsingBufferedReader("src/main/kotlin/_2017/day1/input.txt")
+    val first = file[0].substring(0, file[0].length / 2)
+    val second = file[0].substring(file[0].length / 2 )
+    val result = first.zip(second).filter { it.first == it.second }.map { 2 * Integer.parseInt(it.first.toString()) }.sum()
+    println(result)
+}
